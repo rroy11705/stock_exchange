d2fc5f0a44f361c2e2c0f134eecaf6c426330a85
YOUR_USERNAMErroy11705 <rroy11705@gmail.com>
2021-10-13 14:11:29 +0530

    scraper added

--- scraper/getStockPrice.py
+++ scraper/getStockPrice.py
@@ -1,53 +1,95 @@
 import requests
 from bs4 import BeautifulSoup
+import re
+from multiprocessing.pool import Pool
+from concurrent.futures import ThreadPoolExecutor
+from timeit import default_timer as timer
+from datetime import timedelta
+
+from utils import replace_data
+
 
 headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'}
 
-def getData(symbol):
-    url = 'https://finance.yahoo.com/quote/'+ symbol + '?p=' + symbol
-    r = requests.get(url, headers=headers, timeout=5).text 
 
-    soup = BeautifulSoup(r, 'html.parser')
+def saveData(stock):
 
-    price = soup.find('div', {'class': 'D(ib) Mend(20px)'}).find_all('span')[0].text
-    change = soup.find('div', {'class': 'D(ib) Mend(20px)'}).find_all('span')[1].text
-    data = soup.find_all('td', {'class': 'Ta(end) Fw(600) Lh(14px)'})
-    prev_close_value = data[0].text
-    open_value = data[1].text
-    days_range = data[4].text
-    fifty_two_week_range = data[5].text
-    volume = data[6].text
-    avg_volume = data[7].text
-    market_cap = data[8].text
-    dividend = data[13].text
-    prev_dividend_date = data[14].text
+    try:
+        url = 'https://finance.yahoo.com/quote/' + stock[1] + '?p=' + stock[1]
+        r = requests.get(url, headers=headers, timeout=5).text 
 
-    print(symbol, price, change, prev_close_value, open_value, days_range)
+        soup = BeautifulSoup(r, 'html.parser')
+
+        price = soup.find('div', {'class': 'D(ib) Mend(20px)'}).find_all('span')[0].text
+        change = soup.find('div', {'class': 'D(ib) Mend(20px)'}).find_all('span')[1].text
+        change_amount = (change.split(' ')[0])
+        change_percent = re.sub('[()]', '', change.split(' ')[1])
+        data = soup.find_all('td', {'class': 'Ta(end) Fw(600) Lh(14px)'})
+        prev_close_value = data[0].text
+        open_value = data[1].text
+        days_range = data[4].text
+        fifty_two_week_range = data[5].text
+        volume = data[6].text
+        avg_volume = data[7].text
+        market_cap = data[8].text
+        dividend = data[13].text
+        prev_dividend_date = data[14].text
+
+        replace_data(stock[1], stock[0], price, change_amount, change_percent, prev_close_value,
+            open_value, days_range, fifty_two_week_range, 
+            volume, avg_volume, market_cap, dividend, prev_dividend_date)
+
+    except Exception as e:
+        print("Error", e)
 
 
-def getMostActiveSocks():
-    url = 'https://finance.yahoo.com/screener/unsaved/3f29f493-e529-4abd-852c-5a773fb8a7a4'
+def getMostActiveSocks(offset, count):
+
+    url = f'https://finance.yahoo.com/screener/unsaved/3f29f493-e529-4abd-852c-5a773fb8a7a4?count={count}&offset={offset}'
     r = requests.get(url, headers=headers, timeout=5).text
 
     soup = BeautifulSoup(r, 'html.parser')
 
     name = soup.find_all('td', {'class': 'Va(m) Ta(start) Px(10px) Fz(s)'})
     symbol = soup.find_all('a', {'class': 'Fw(600) C($linkColor)'})
-
+    
     for i in range(0, len(name)):
         yield (name[i].text, symbol[i].text)
 
 
-if __name__ == '__main__':
-    # stockSymbols = ['ASPL.L', 'AAPL', 'TCS.NS']
-    # for symbol in stockSymbols:
-    #     getData(symbol)
-    stocks = getMostActiveSocks()
-
+def fetchData(offset, count):
+    stocks = getMostActiveSocks(offset, count)
+    c = offset
     while True:
         try:
             stock = next(stocks)
-            getData(stock[1])
+            c += 1
+            print(c, "Adding data of", stock[1])
+            saveData(stock)
 
         except StopIteration:
             break
+
+
+# multiprocessing    took 18s
+# def main():
+#     with Pool() as pool:
+#         pool.starmap(fetchData, [(x*100, 100) for x in range(0, 59)])
+
+# multithreading     took 11s
+def main():
+    with ThreadPoolExecutor(max_workers=30) as executor:
+        executor.map(fetchData, [x*100 for x in range(0, 59)], [100] * 59)
+        executor.shutdown(wait=True)
+
+# synchronous     took 1min 34s
+# def main():
+#     for i in range(0, 59):
+#         fetchData(i*100, 100)
+
+
+if __name__ == '__main__':
+    start = timer()
+    main()
+    end = timer()
+    print(timedelta(seconds=end-start))
---
+++ scraper/utils.py
@@ -0,0 +1,63 @@
+from re import A
+import psycopg2
+
+try:
+    con = psycopg2.connect(
+        host="ec2-34-203-91-150.compute-1.amazonaws.com",
+        database="d33iakhgr3burt",
+        user="yzwpvnpxqsdpqa",
+        port="5432",
+        password="08e2b09acc992e591c8de525390e61b2e1c256d971b191bc9b0803eb112422d7")
+
+except Exception as e:
+    print("Error:", e)
+
+# symbol, name, price, change_percent, change_percent, prev_close_value,
+# open_value, days_range, fifty_two_week_range,
+# volume, avg_volume, market_cap, dividend, prev_dividend_date
+
+def create_table():
+    cur = con.cursor()
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS core_live_stocks
+    (
+        symbol VARCHAR PRIMARY KEY NOT NULL,
+        name VARCHAR NOT NULL,
+        price VARCHAR NOT NULL,
+        change VARCHAR NOT NULL,
+        change_percent VARCHAR NOT NULL,
+        prev_close_value VARCHAR NULL,
+        open_value VARCHAR NULL,
+        days_range VARCHAR NULL,
+        fifty_two_week_range VARCHAR NULL,
+        volume VARCHAR NULL,
+        avg_volume VARCHAR NULL,
+        market_cap VARCHAR NULL,
+        dividend VARCHAR NULL,
+        prev_dividend_date VARCHAR NULL
+    )
+    """)
+
+    cur.close()
+
+    con.commit()
+    print("Table Live Stocks created!")
+
+
+def replace_data(*args):
+    cur = con.cursor()
+    cur.execute("""
+    INSERT INTO
+    core_live_stocks (symbol, name, price, change, change_percent, prev_close_value, open_value, days_range, fifty_two_week_range, volume, avg_volume, market_cap, dividend, prev_dividend_date)
+    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+    ON CONFLICT (symbol) DO
+    UPDATE SET 
+    name = %s, price = %s, change = %s, change_percent = %s, prev_close_value = %s, open_value = %s, days_range = %s, fifty_two_week_range = %s, volume = %s, avg_volume = %s, market_cap = %s, dividend = %s, prev_dividend_date = %s;
+    """, (args + args[1:]))
+
+    con.commit()
+    print(f"data of {args[0]} saved in Live Stocks!")
+
+
+if __name__ == "__main__":
+    create_table()
