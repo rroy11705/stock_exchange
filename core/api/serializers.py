1866ffee1579345a66182b9e2efcf52eb48b86c1
YOUR_USERNAMErroy11705 <rroy11705@gmail.com>
2021-10-18 13:36:43 +0530

    requirements.txt updated

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -9,5 +9,5 @@ class LiveStocksSerializer(serializers.ModelSerializer):
         fields = ['symbol', 'name', 'price', 'change_amount', 'change_percent', 'prev_close_value', 'open_value', 'bid_value', 'bid_quantity', 'ask_value', 'ask_quantity', 'days_high', 'days_low', 'fifty_two_week_high', 'fifty_two_week_low', 'volume', 'avg_volume', 'market_cap', 'pe_ratio', 'eps_ratio', 'forward_dividend_yield']
 
     def get_change_percent(self, obj):
-        change_percent = obj.change_amount * 100 / obj.price
+        change_percent = round(obj.change_amount * 100 / obj.prev_close_value, 2)
         return change_percent
