d743b80f620d989c71c827a324a4ae2d50947853
YOUR_USERNAMErroy11705 <rroy11705@gmail.com>
2021-10-19 18:50:19 +0530

    frontend build, first draft

--- backend/settings.py
+++ backend/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'https://stockexchangehelper.herokuapp.com/']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'stockexchangehelper.herokuapp.com', 'www.stockexchangehelper.herokuapp.com']
 
 
 # Application definition
